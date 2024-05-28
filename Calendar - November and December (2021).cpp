f01b4a10ec0e2911f936144a165232acf2a7bd92
Erica Borser <erica.borser.09@gmail.com>
2024-05-28 08:39:43 +0800

    Add files via upload

---
+++ Calendar - November and December (2021).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"\t\t\t November \t\t\t\t\t\t\t December \n";
+cout<<"S \t M \t T \t W \t T \t F \t S \t\t S \t M \t T \t W \t T \t F \t S \n";
+cout<<"\t 1 \t 2 \t 3 \t 4 \t 5 \t 6 \t\t\t\t\t 1 \t 2 \t 3 \t 4 \n";
+cout<<"7 \t 8 \t 9 \t 10 \t 11 \t 12 \t 13 \t\t 5 \t 6 \t 7 \t 8 \t 9 \t 10 \t 11 \n";
+cout<<"14 \t 15 \t 16 \t 17 \t 18 \t 19 \t 20 \t\t 12 \t 13 \t 14 \t 15 \t 16 \t 17 \t 18 \n";
+cout<<"21 \t 22 \t 23 \t 24 \t 25 \t 26 \t 27 \t\t 19\t 20 \t 21 \t 22 \t 23 \t 24 \t 25 \n";
+cout<<"28 \t 29 \t 30 \t\t\t\t\t\t 26 \t 27 \t 28 \t 29 \t 30 \t 31";
+	
+return 0;
+}
