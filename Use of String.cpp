712eb503c787b1ea5be704ba6a1ba0663d7b2466
Erica Borser <erica.borser.09@gmail.com>
2024-05-28 08:53:35 +0800

    Add files via upload

---
+++ Use of String.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string num1, num2, num3;
+	cout<<"Type your first name: ";
+	cin>>num1;
+	cout<<"Type your middle name: ";
+	cin>>num2;
+	cout<<"Type your last name: ";
+	cin>>num3;
+	cout<<"Hello "<<num1<<" "<<num2<<" "<<num3<<"!";
+	
+	return 0;
+}
