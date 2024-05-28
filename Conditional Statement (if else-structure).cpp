b85b369999e3385af87c5c3a2a5127f24d3b4dea
Erica Borser <erica.borser.09@gmail.com>
2024-05-28 09:22:12 +0800

    Add files via upload

---
+++ Conditional Statement (if else-structure).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	cout<<"Type a number: ";
+	cin>>x;
+	
+	if(x<200){
+		cout<<"The number is lower than 200.\n";
+	}
+	else{
+		cout<<"The number is equal or greater than 200.\n";
+	}
+	
+	cout<<"End of the Program.";
+	
+	return 0;
+}
