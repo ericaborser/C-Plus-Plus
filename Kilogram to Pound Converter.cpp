224652d9556de4d1f98459602debbf59070601ab
Erica Borser <erica.borser.09@gmail.com>
2024-05-28 08:57:56 +0800

    Add files via upload

---
+++ Kilogram to Pound Converter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float kl, result;
+	cout<<"Type the kilogram: ";
+	cin>>kl;
+    result= kl*2.2;
+	cout<<"There are "<<result <<" pounds in "<<kl<<" kilogram.";
+	
+	return 0;
+}
