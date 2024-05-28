99b9c39bb5b2b7b0b7769ce5794bb39f1f16e397
Erica Borser <erica.borser.09@gmail.com>
2024-05-28 08:37:43 +0800

    Add files via upload

---
+++ Introduction for C++.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\t\t\t\t\t\t Rizal Technological University \n";
+	cout<<"\t\t\t\t\t\t College of Arts and Sciences \n";
+	cout<<"\t\t\t\t\t\t Bachelor of Science in Statistics \n";
+	cout<<"\t\t\t\t\t\t Welcome to Basic Computer Languanges";
+	
+	return 0;
+}
