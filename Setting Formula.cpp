012aa8ce3efd3cfa0b2aaa54de9f21f36ab35d5d
Erica Borser <erica.borser.09@gmail.com>
2024-05-28 08:47:27 +0800

    Add files via upload

---
+++ Setting Formula.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a=10, b=20, c=5, d=15, f=15, i=2, result=0;
+	cout<<" The formula is (a+b/c-15/d+30/f-5*i) \n";
+	result=(a+b/c-15/d+30/f-5*i);
+	cout<<" The result is "<<result<<".";
+	
+	return 0;
+}
